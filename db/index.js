8e37b26e223fc4549f37fb5e8dbe033a3249668b
Karel Fourie <karelmakazi@gmail.com>
2020-06-12 11:23:58 +1200

    Configure Postgres pool

---
+++ db/index.js
@@ -0,0 +1,9 @@
+const { Pool } = require('pg')
+const { user, host, database, password, port } = require('../secrets/db_configuration')
+
+const pool = new Pool({user, host, database, password, port})
+
+pool.query('SELECT * FROM monsters', (err, res) => {
+  if (err) console.log(err)
+  console.log(res)
+})
