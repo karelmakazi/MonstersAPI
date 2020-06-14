d50e3a68b9344ed2bba3618b7a9c79a134c8e1e3
Karel Fourie <karelmakazi@gmail.com>
2020-06-15 09:21:43 +1200

    Improve error handling

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const pool = require('./db')
 
 const app = express()
 
-app.get('/monsters', (request, response) => {
-  pool.query('SELECT * FROM monsters ORDER BY id ASC', (err, res) => {
-    if (err) console.log(err)
+app.get('/monsters', (request, response, next) => {
+  pool.query('SELECT * FROM monstero ORDER BY id ASC', (err, res) => {
+    if (err) return next(err)
     response.json(res.rows)
   })
 })
 
-module.exports = app  
+app.use((err, req, res, next) => {
+  res.json(err)
+})
+
+module.exports = app   
